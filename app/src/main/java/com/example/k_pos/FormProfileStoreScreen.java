7585e83d51a0dd93ad9e7bacfa10b7dd95fe5c8c
Fritzent <afritzent@gmail.com>
2020-05-01 14:06:27 +0700

    "Develop Main Screen"

---
+++ app/src/main/java/com/example/k_pos/FormProfileStoreScreen.java
@@ -0,0 +1,60 @@
+package com.example.k_pos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class FormProfileStoreScreen extends AppCompatActivity {
+
+    Button btnForward;
+    Dialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_profile_store_screen);
+
+        getSupportActionBar().setTitle("Profile Store");
+
+        btnForward = findViewById(R.id.btnForward);
+
+        btnForward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    ShowDialog("Register Success!","Check your email register for the complete information");
+            }
+        });
+    }
+    private void ShowDialog(String txt1, String txt2){
+        TextView txtAlert,txtAlertInfo;
+        ImageView imgAlert;
+
+        dialog = new Dialog(FormProfileStoreScreen.this);
+        dialog.setContentView(R.layout.custome_dialog_register_complete);
+        dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        Window window = dialog.getWindow();
+        window.setGravity(Gravity.CENTER);
+        window.getAttributes().windowAnimations = R.style.DialogAnimation;
+
+        txtAlert = dialog.findViewById(R.id.textAlert);
+        txtAlertInfo = dialog.findViewById(R.id.textAlertInformation);
+        imgAlert = dialog.findViewById(R.id.successLogo);
+
+        txtAlert.setText(txt1);
+        txtAlertInfo.setText(txt2);
+
+        imgAlert.setImageResource(R.drawable.iconsuccess);
+
+        dialog.setCancelable(true);
+        window.setLayout(ActionBar.LayoutParams.WRAP_CONTENT,ActionBar.LayoutParams.WRAP_CONTENT);
+        dialog.show();
+    }
+}
