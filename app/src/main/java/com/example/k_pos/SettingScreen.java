48b852b98d485c276936e9c23fe7e403e8c96417
Fritzent <afritzent@gmail.com>
2020-05-04 02:01:51 +0700

    "Add Setting Screen "

---
+++ app/src/main/java/com/example/k_pos/SettingScreen.java
@@ -0,0 +1,16 @@
+package com.example.k_pos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_screen);
+
+        getSupportActionBar().hide();
+    }
+}
